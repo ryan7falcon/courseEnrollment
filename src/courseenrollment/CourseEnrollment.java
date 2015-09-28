d3d9d2d3b2e842b04c65a17b11b5412d1ff39d37
Ryan West <galiya991@gmail.com>
2015-09-28 14:20:45 -0700

    all done

--- src/courseenrollment/CourseEnrollment.java
+++ src/courseenrollment/CourseEnrollment.java
@@ -1,7 +1,7 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * CourseEnrollment.java
+ *----------------------
+ * Accesses the database and prints a list of all students and students enrolled in certain courses
  */
 package courseenrollment;
 
@@ -13,8 +13,6 @@ import java.sql.ResultSet;
  * @author Ryan West (Galina Galimova) (galiya991@gmail.com)
  */
 public class CourseEnrollment {
-
-    
     /**
      * @param args the command line arguments
      */
@@ -22,57 +20,62 @@ public class CourseEnrollment {
        //create instance of JDBC helper
         JdbcHelper jdbc = new JdbcHelper();
         
-        //test connection: location, user name, password
-        boolean connected = jdbc.connect("jdbc:mysql://localhost:3306/ejd", 
+        //connection: location, user name, password
+        jdbc.connect("jdbc:mysql://localhost:3306/ejd", 
                                         "root", "dataPASS");
    
-        //queries
+        //query to get a list of all students
         String q0 = "SELECT * FROM Student";
-        printStudentList("", q0, jdbc);
+        //getting the result and printing 
+        printStudentList("Students", true, q0, jdbc); 
         
-        String c1 = "MATH10000";
-        String q1 = "SELECT firstName, lastName FROM Student "
-                + "INNER JOIN CourseStudent ON student.id = CourseStudent.studentId "
-                + "WHERE CourseStudent.courseId = " + "'" + c1 + "'";
-        printStudentList(c1, q1, jdbc);
-        
-        String c2 = "PROG30000";
-        String q2 =  "SELECT firstName, lastName FROM Student "
-                + "INNER JOIN CourseStudent ON student.id = CourseStudent.studentId "
-                + "WHERE CourseStudent.courseId = " + "'" + c2 + "'";
-        printStudentList(c2, q2, jdbc);
+        //getting and printing the list of students in a particular course
+        printStudentsInCourse("MATH10000", jdbc);
+        printStudentsInCourse("PROG30000", jdbc);
         
         //disconnect from the database
         jdbc.disconnect();
     }
     
-    //prints a list, obtained from the querym 
-    private static void printStudentList(String course, String query, JdbcHelper jdbc){
-        String s = "";
-        if (course!=""){
-            s = " in " + course;
-        }
+    //gets and print a list of students using a given query
+    //params: a header of the table, boolean deisplayId - display id of a student or not,
+    //a query, instance of JdbcHelper
+    private static void printStudentList(String header, boolean displayId, String query, JdbcHelper jdbc){
+        
         try{
+            //get a list
             ResultSet result = jdbc.query(query);
-            System.out.println("Students" + s);
+            //header
+            System.out.println(header);
             System.out.println("==================");
+           
             String id = "";
             while(result.next()){
-                //get 3 field fromeach row
-                if (s=="") {id = result.getString("id");}
+                //id is printed only if displayId is true
+                if (displayId) {id = result.getString("id");}
+                //getting first name and last name
                 String first = result.getString("firstName"); 
                 String last = result.getString("lastName");
-                
-                //print
-                if (s=="") {System.out.printf("%9s:", id);}
+                //printing the result
+                if (displayId) {System.out.printf("%9s:", id);}
                 System.out.printf(" %s %s\n", first, last);
             }
+            //formatting
             System.out.println("==================");
             System.out.println("\n");
         }
+        //handling exceptions
         catch(Exception e){
-            System.out.println(e.getMessage());
+            System.err.println(e.getMessage());
         }
     }
     
+    //gets and prints a list of students in a given course
+    //params: courseId and instance of JdbcHelper
+    private static void printStudentsInCourse(String course, JdbcHelper jdbc){
+        String q = "SELECT firstName, lastName FROM Student "
+                + "INNER JOIN CourseStudent ON student.id = CourseStudent.studentId "
+                + "WHERE CourseStudent.courseId = " + "'" + course + "'";
+        printStudentList("Students in " + course, false, q, jdbc); 
+    }
 }
