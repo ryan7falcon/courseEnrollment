4babb3731cb3680d539f44c535c40e4e75aa848d
Ryan West <galiya991@gmail.com>
2015-09-28 15:45:49 -0700

    prepareStatement

--- src/courseenrollment/CourseEnrollment.java
+++ src/courseenrollment/CourseEnrollment.java
@@ -7,6 +7,7 @@ package courseenrollment;
 
 import ejd.JdbcHelper;
 import java.sql.ResultSet;
+import java.util.ArrayList;
 
 /**
  *
@@ -27,7 +28,7 @@ public class CourseEnrollment {
         //query to get a list of all students
         String q0 = "SELECT * FROM Student";
         //getting the result and printing 
-        printStudentList("Students", true, q0, jdbc); 
+        printStudentList("Students", true, q0, null, jdbc); 
         
         //getting and printing the list of students in a particular course
         printStudentsInCourse("MATH10000", jdbc);
@@ -40,11 +41,17 @@ public class CourseEnrollment {
     //gets and print a list of students using a given query
     //params: a header of the table, boolean deisplayId - display id of a student or not,
     //a query, instance of JdbcHelper
-    private static void printStudentList(String header, boolean displayId, String query, JdbcHelper jdbc){
+    private static void printStudentList(String header, boolean displayId, String query, ArrayList<Object> params, JdbcHelper jdbc){
         
         try{
+            ResultSet result;
             //get a list
-            ResultSet result = jdbc.query(query);
+            if (params!=null){ //if this is a list of students for a certain course
+                result = jdbc.query(query, params);}
+            else{ //if this is a list of all students
+                result = jdbc.query(query);
+            }
+                
             //header
             System.out.println(header);
             System.out.println("==================");
@@ -75,7 +82,10 @@ public class CourseEnrollment {
     private static void printStudentsInCourse(String course, JdbcHelper jdbc){
         String q = "SELECT firstName, lastName FROM Student "
                 + "INNER JOIN CourseStudent ON student.id = CourseStudent.studentId "
-                + "WHERE CourseStudent.courseId = " + "'" + course + "'";
-        printStudentList("Students in " + course, false, q, jdbc); 
+                + "WHERE CourseStudent.courseId = ?";
+        //parameter for prepareStatement
+         ArrayList<Object> params = new ArrayList<Object>();
+         params.add(course);
+        printStudentList("Students in " + course, false, q, params, jdbc); 
     }
 }
--- src/ejd/JdbcHelper.java
+++ src/ejd/JdbcHelper.java
@@ -12,7 +12,9 @@ import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.Statement;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 
 public class JdbcHelper {
@@ -23,11 +25,21 @@ public class JdbcHelper {
     private ResultSet resultSet;
     String errorMessage;
     private boolean connected = false;
-    
-    //constructor
+    //for prepared statement
+    PreparedStatement activeStatement;
+    String activeSql;
     
     public boolean isConnected(){return connected;}
     
+    public JdbcHelper(){
+        connection = null;
+        statement = null;
+        resultSet = null;
+        activeSql = "";
+        activeStatement = null;
+        errorMessage = "";
+        
+    }
     
     //connect to database with 3 params: URL, username, password
     //returns true if success, otherwise returns false
@@ -89,6 +101,34 @@ public class JdbcHelper {
         return this.resultSet;
     }
     
+    //executes a sql query
+    public ResultSet query(String sql,  ArrayList<Object> params){
+        //reset
+        errorMessage = "";
+        resultSet = null;
+        
+        try{
+            //create prerared statement only if it doesn`t exist
+            if(!activeSql.equals(sql)){
+                activeStatement = connection.prepareStatement(sql);
+                activeSql = sql; //update 
+            }
+            //set parameters for prepared statement (down-cast from object to specific datatype)
+            setParametersForPreparedStatement(params);
+            //execute prerarred statement
+            resultSet = activeStatement.executeQuery();
+        }
+        catch(SQLException e){
+           errorMessage = "[ERROR]" + e.getSQLState() + e.getMessage();
+           System.err.println(errorMessage);
+        }
+        catch(Exception e){
+            errorMessage = "[ERROR]" + e.getMessage();
+            System.err.println(errorMessage);
+        }
+        return this.resultSet;
+    }
+    
     //executes a sql statement and returns zero or # of rows updated 
     //if failed, returns -1
     public int update(String sql){
@@ -109,6 +149,59 @@ public class JdbcHelper {
         return result;
     }
     
+    //execuding three steps whithing one method
+    public int update(String sql, ArrayList<Object> params){
+        
+        int result = -1; //default return value
+        errorMessage = ""; 
+        
+        try{
+            //create prerared statement only if it doesn`t exist
+            if(!activeSql.equals(sql)){
+                activeStatement = connection.prepareStatement(sql);
+                activeSql = sql; //update 
+            }
+            //set parameters for prepared statement (down-cast from object to specific datatype)
+            setParametersForPreparedStatement(params);
+            //execute prerarred statement
+            result = activeStatement.executeUpdate();
+        }
+        catch(SQLException e){
+            errorMessage = "[ERROR] "  + e.getSQLState() + ": " + e.getMessage();
+            System.err.println(errorMessage);
+        }
+        catch(Exception e){
+             errorMessage = "[ERROR] " + e.getMessage();
+             System.err.println(errorMessage);
+        }
+        //if it is success, it will return prorer value, othervise will be exception throun
+        return result;
+    }
+    
+    //down-cast all params to original type then call setXXX()
+    private void setParametersForPreparedStatement(ArrayList<Object> params){
+        errorMessage = "";
+        Object param = null;
+        try{
+            for(int i = 1; i <= params.size(); i++){
+                param = params.get(i - 1);
+                if(param instanceof Integer)
+                    activeStatement.setInt(i, (int)param);
+                else if(param  instanceof Double)
+                    activeStatement.setDouble(i, (double)param);
+                else if(param  instanceof String)
+                    activeStatement.setString(i, (String)param);
+            }
+        }
+        catch(SQLException e){
+            errorMessage = "[ERROR] "  + e.getSQLState() + ": " + e.getMessage();
+            System.err.println(errorMessage);
+        }
+        catch(Exception e){
+             errorMessage = "[ERROR] " + e.getMessage();
+             System.err.println(errorMessage);
+        }
+    }
     //returns the current error message
     public String getErrorMessage(){
         return errorMessage;
